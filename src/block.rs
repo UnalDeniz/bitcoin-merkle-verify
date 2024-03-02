722b52c895f416827f5b7192ed8e48ac25ac87b6
Deniz Unal <den1z.unal@protonmail.com>
2024-03-02 19:23:37 +0300

    added documentation

--- src/block.rs
+++ src/block.rs
@@ -1,11 +1,13 @@
 use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
 
+/// A block in the blockchain.
 pub struct Block {
     header: Header,
     transactions: Vec<Transaction>,
 }
 
+/// The header of a block.
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Header {
     version: u32,
@@ -16,6 +18,7 @@ pub struct Header {
     previousblockhash: String,
 }
 
+/// A transaction in a block.
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Transaction {
     txid: String,
@@ -29,6 +32,7 @@ pub struct Transaction {
 }
 
 impl Block {
+    /// Finds the wtxid root from the first transaction.
     fn find_wtxid_root(&self) -> Result<String, &'static str> {
         for vout in &self.transactions[0].vout {
             if let Some(hex) = vout.script_pub_key.hex.strip_prefix("6a24aa21a9ed") {
@@ -38,47 +42,7 @@ impl Block {
         Err("Could not find wtxid root in transaction")
     }
 
-    fn merkle_proof(root: String, txid_list: Vec<String>) -> bool {
-        let mut hashes: Vec<String> = Vec::new();
-
-        for hash in txid_list {
-            hashes.push(convert_endianness(&hash.clone()));
-        }
-
-        while hashes.len() > 1 {
-            if hashes.len() % 2 != 0 {
-                hashes.push(hashes.last().unwrap().clone());
-            }
-
-            let mut new_hashes = Vec::new();
-
-            for i in 0..hashes.len() / 2 {
-                let hasher = Sha256::new();
-
-                let mut first = hex::decode(hashes[i * 2].clone()).unwrap();
-
-                let second = hex::decode(hashes[i * 2 + 1].clone()).unwrap();
-
-                first.extend(second);
-
-                let hash = hash_binary(first, hasher.clone());
-
-                let second_hash = hash_binary(hex::decode(hash).unwrap(), hasher);
-
-                new_hashes.push(second_hash);
-            }
-
-            hashes = new_hashes;
-        }
-
-        let calculated = convert_endianness(&hashes[0]);
-
-        println!("Merkle root: {}", calculated);
-        println!("Root: {}", root);
-
-        return calculated == root;
-    }
-
+    /// Checks if the block is valid by verifying the merkle root against all txids and wtxid merkle root against wtxids.
     pub fn verify(&self) -> bool {
         let wtxid_root = self.find_wtxid_root().unwrap();
         let txids = self
@@ -92,15 +56,20 @@ impl Block {
             .map(|x| x.hash.clone())
             .collect::<Vec<String>>();
 
-        if !Block::merkle_proof(self.header.merkleroot.clone(), txids) {
+        if !merkle_proof(self.header.merkleroot.clone(), txids) {
             println!("Merkle proof for txids failed");
+            println!("The transactions does not belong to the given header.");
             return false;
         }
+        println!("Merkle proof for txids is successful.");
 
-        if !Block::merkle_proof(wtxid_root, wtxids) {
-            println!("Merkle proof wtxids failed");
+        if !merkle_proof(wtxid_root, wtxids) {
+            println!("Merkle proof for wtxids failed");
+            println!("The transactions does not belong to the given header.");
             return false;
         }
+        println!("Merkle proof for wtxids is successful.");
+        println!("The transactions belong to the given header.");
         true
     }
 
@@ -112,6 +81,55 @@ impl Block {
     }
 }
 
+/// Calculates the merkle root and verifies it against the given root.
+fn merkle_proof(root: String, txid_list: Vec<String>) -> bool {
+    let mut hashes: Vec<String> = Vec::new();
+
+    // Convert all txids to little-endian.
+    for hash in txid_list {
+        hashes.push(convert_endianness(&hash.clone()));
+    }
+
+    // Continue until there is only one hash left.
+    while hashes.len() > 1 {
+        // If the number of hashes is odd, duplicate the last hash.
+        if hashes.len() % 2 != 0 {
+            hashes.push(hashes.last().unwrap().clone());
+        }
+
+        let mut new_hashes = Vec::new();
+
+        for i in 0..hashes.len() / 2 {
+            let hasher = Sha256::new();
+
+            // Concatenate the two hashes.
+            let mut first = hex::decode(hashes[i * 2].clone()).unwrap();
+
+            let second = hex::decode(hashes[i * 2 + 1].clone()).unwrap();
+
+            first.extend(second);
+
+            // Double hash the concatenated hashes.
+            let hash = hash_binary(first, hasher.clone());
+
+            let second_hash = hash_binary(hex::decode(hash).unwrap(), hasher);
+
+            new_hashes.push(second_hash);
+        }
+
+        hashes = new_hashes;
+    }
+
+    // Convert the calculated merkle root to big-endian.
+    let calculated = convert_endianness(&hashes[0]);
+
+    println!("The calculated merkle root is: {}", calculated);
+    println!("The given merkle root is: {}", root);
+
+    return calculated == root;
+}
+
+/// Creates a SHA-256 hash from a binary data.
 fn hash_binary(data: Vec<u8>, mut hasher: Sha256) -> String {
     hasher.update(data);
 
@@ -120,11 +138,15 @@ fn hash_binary(data: Vec<u8>, mut hasher: Sha256) -> String {
     format!("{:x}", result)
 }
 
+/// Converts the endianness of a hexadecimal string.
 fn convert_endianness(hex_string: &str) -> String {
+    // Convert the hexadecimal string to bytes.
     let bytes_data = hex::decode(hex_string).expect("Invalid hexadecimal string");
 
+    // Reverse the bytes.
     let reversed_bytes: Vec<u8> = bytes_data.iter().rev().cloned().collect();
 
+    // Convert the reversed bytes to a hexadecimal string.
     let reversed_hex_string = hex::encode(reversed_bytes);
     reversed_hex_string
 }
@@ -144,6 +166,7 @@ struct ScriptSig {
     hex: String,
 }
 
+/// The input of a transaction.
 #[derive(Serialize, Deserialize, Debug)]
 struct Vin {
     coinbase: Option<String>,
@@ -154,6 +177,7 @@ struct Vin {
     sequence: u32,
 }
 
+/// The output of a transaction.
 #[derive(Serialize, Deserialize, Debug)]
 struct Vout {
     value: f64,
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+#![warn(missing_docs)]
+
+//! A simple blockchain verification in Rust.
+
 use crate::block::{Block, Header, Transaction};
 use std::env;
 use std::fs::File;
@@ -5,9 +9,11 @@ use std::io::BufReader;
 
 mod block;
 
+/// Reads a header and a list of transactions from JSON files and verifies the block.
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    // Check if the number of arguments is correct.
     if args.len() != 3 {
         eprintln!("Usage: {} <header-json> <transaction-json>", args[0]);
         return;
@@ -15,18 +21,23 @@ fn main() {
 
     let file = File::open(&args[1]).expect("Failed to open file");
 
+    // Create a new buffered reader to read the file.
     let reader = BufReader::new(file);
 
+    // Deserialize the JSON file into a Header struct.
     let header: Header = serde_json::from_reader(reader).expect("Failed to parse JSON");
 
     let file = File::open(&args[2]).expect("Failed to open file");
 
     let reader = BufReader::new(file);
 
+    // Deserialize the JSON file into a Transaction struct.
     let transactions: Vec<Transaction> =
         serde_json::from_reader(reader).expect("Failed to parse JSON");
 
+    // Create a new block with the header and transactions.
     let block = Block::new(header, transactions);
 
+    // Verify the block.
     block.verify();
 }
